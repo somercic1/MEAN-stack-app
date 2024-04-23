fab3da91f64319f13cadb9613eeb991e0db309e5
somercic1 <somercic1@etf.unsa.ba>
2024-04-23 23:00:05 +0200

    Redirecting users to home page while login or logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { AuthData } from "./auth-data.model";
 import { response } from "express";
 import { Subject } from "rxjs";
+import { Router } from "@angular/router";
 
 @Injectable({ providedIn: "root"})
 export class AuthService {
@@ -10,7 +11,7 @@ export class AuthService {
     private token: string;
     private authStatusListener = new Subject<boolean>();
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private router: Router) {}
 
     getToken() {
         return this.token;
@@ -40,7 +41,16 @@ export class AuthService {
             if (token) {
                 this.isAuthenticated = true;
                 this.authStatusListener.next(true);
+                this.router.navigate(['/']);
             }
-        })
+        });
     }
+
+    logout() {
+        this.token = null;
+        this.isAuthenticated = false;
+        this.authStatusListener.next(false);
+        this.router.navigate(['/']);
+    }
+
 }
