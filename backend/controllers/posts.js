97f3bc377e48c51cc83703ab0f9624de3438b055
somercic1 <somercic1@etf.unsa.ba>
2024-04-25 01:40:23 +0200

    Fixed bug with update/edit post

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -41,7 +41,7 @@ exports.updatePost = (req, res, next) => {
       { _id: req.params.id, creator: req.userData.userId },
        post
       ).then(result => {
-      if (result.modifiedCount > 0) {
+      if (result.matchedCount > 0) {
         res.status(200).json({ message: "Update successful!" });
       } else {
         res.status(401).json({ message: "Not authorized!" });
