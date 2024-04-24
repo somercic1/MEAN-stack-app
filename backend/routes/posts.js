12cca008d2fa5c506af7a4255e24e4e404295fe9
somercic1 <somercic1@etf.unsa.ba>
2024-04-24 19:19:50 +0200

    Handling errors finished

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -51,6 +51,11 @@ router.post(
           id: createdPost._id
         }
       });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Creating a post failed!"
+      }); 
     });
   }
 );
@@ -72,13 +77,21 @@ router.put(
       imagePath: imagePath,
       creator: req.userData.userId
     });
-    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
+    Post.updateOne(
+      { _id: req.params.id, creator: req.userData.userId },
+       post
+      ).then(result => {
       if (result.modifiedCount > 0) {
         res.status(200).json({ message: "Update successful!" });
       } else {
         res.status(401).json({ message: "Not authorized!" });
       }
       
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Couldn't update post!"
+      });
     });
   }
 );
@@ -102,6 +115,11 @@ router.get("", (req, res, next) => {
         posts: fetchedPosts,
         maxPosts: count
       });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Fetching posts failed!"
+      });
     });
 });
 
@@ -112,16 +130,26 @@ router.get("/:id", (req, res, next) => {
     } else {
       res.status(404).json({ message: "Post not found!" });
     }
+  })
+  .catch(error => {
+    res.status(500).json({
+      message: "Fetching post failed!"
+    });
   });
 });
 
 router.delete("/:id", checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(result => {
+  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(
+    result => {
     if (result.deletedCount > 0) {
       res.status(200).json({ message: "Deletion successful!" });
     } else {
       res.status(401).json({ message: "Not authorized!" });
     }
+  }).catch(error => {
+    res.status(500).json({
+      message: "Fetching posts failed!"
+    });
   });
 });
 
