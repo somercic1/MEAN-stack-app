a3475a5be6d2c4d3b96dde0b52e4bff805d8fae6
somercic1 <somercic1@etf.unsa.ba>
2024-04-24 17:23:37 +0200

    Handling login and signup errors

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -32,15 +32,22 @@ export class AuthService {
     
     createUser(email: string, password: string) {
         const authData: AuthData = {email: email, password: password};
-        this.http.post("http://localhost:3000/api/user/signup", authData)
-        .subscribe(response => {
-            console.log(response);
-        })
+        this.http
+        .post("http://localhost:3000/api/user/signup", authData)
+        .subscribe(() => {
+            this.router.navigate['/'];
+        }, error => {
+            this.authStatusListener.next(false);
+        });
     }
 
     login(email: string, password: string) {
         const authData: AuthData = {email: email, password: password};
-        this.http.post<{ token: string, expiresIn: number, userId: string }>("http://localhost:3000/api/user/login", authData)
+        this.http
+        .post<{ token: string, expiresIn: number, userId: string }>(
+            "http://localhost:3000/api/user/login", 
+            authData
+        )
         .subscribe(response => {
             const token = response.token;
             this.token = token;
@@ -55,6 +62,8 @@ export class AuthService {
                 this.saveAuthData(token, expirationDate, this.userId);
                 this.router.navigate(['/']);
             }
+        }, error => {
+            this.authStatusListener.next(false);
         });
     }
 
